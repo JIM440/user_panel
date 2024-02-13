9953be51c1a18037c2f7b3989c229495932c323f
JIM440 <takemjim43@gmail.com>
2024-02-13 23:30:19 +0100

    more changes

--- src/pages/product_detail/components/AddReview.jsx
+++ src/pages/product_detail/components/AddReview.jsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
+import { FaStar, FaTimes } from 'react-icons/fa';
+
 const AddReview = ({ state, toggleFunc }) => {
+  const [rating, setRating] = useState(0);
+  const [hoverValue, setHoverValue] = useState(null);
+  const [textAreaVal, setTextAreaVal] = useState('');
+
+  const handleStarClick = (value) => {
+    setRating(value);
+  };
+  const handleStarMouseOver = (value) => {
+    setHoverValue(value);
+  };
+  const handleStarMouseLeave = (value) => {
+    setHoverValue(null);
+  };
+  const handleOnchange = (event) => {
+    setTextAreaVal(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    if (rating < 1) {
+      event.preventDefault();
+      return;
+    }
+    alert(
+      `You rated the product ${rating} stars and the message was *${textAreaVal}*`
+    );
+  };
   return (
     <div className={`add-review-container ${state ? 'open' : ''}`}>
-      <form action="" className="add-review-form">
+      <form action="" onSubmit={handleSubmit} className="add-review-form">
         <span
           className="close-review"
           onClick={() => {
+            setTextAreaVal('');
+            setRating(0);
             toggleFunc();
           }}
         >
-          <i class="bx bx-x"></i>
+          <FaTimes />
         </span>
         <div className="user-details">
           <span className="letter">T</span>
@@ -16,11 +46,26 @@ const AddReview = ({ state, toggleFunc }) => {
         </div>
         <h3>Rate your experince</h3>
         <div className="stars">
-          <i className="bx bx-star"></i>
-          <i className="bx bx-star"></i>
-          <i className="bx bx-star"></i>
-          <i className="bx bx-star"></i>
-          <i className="bx bx-star"></i>
+          {
+            //create an array with 5 untitled elements
+            [...Array(5)].map((star, index) => {
+              return (
+                <FaStar
+                  key={index}
+                  onClick={() => {
+                    handleStarClick(index + 1);
+                  }}
+                  onMouseLeave={() => {
+                    handleStarMouseLeave(index + 1);
+                  }}
+                  onMouseOver={() => {
+                    handleStarMouseOver(index + 1);
+                  }}
+                  color={(hoverValue || rating) > index ? 'gold' : '#e4e5e9'}
+                />
+              );
+            })
+          }
         </div>
         <textarea
           name=""
@@ -28,9 +73,18 @@ const AddReview = ({ state, toggleFunc }) => {
           cols="30"
           rows="10"
           placeholder="Describe your experience with us"
+          value={textAreaVal}
           required
+          onChange={handleOnchange}
         ></textarea>
-        <button type="submit" className="btn">
+        <button
+          type="submit"
+          className="btn"
+          style={{
+            backgroundColor: `${rating < 1 ? '#e4e5e9' : '#2997ff'}`,
+            cursor: `${rating < 1 ? 'unset' : 'pointer'}`,
+          }}
+        >
           Submit Review
         </button>
       </form>
