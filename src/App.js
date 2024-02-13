1f76140e71efbca8c4ab1ce6592f5dc9dfe4c124
JIM440 <takemjim43@gmail.com>
2024-02-13 09:11:06 +0100

    more changes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 // pages
-import About from './pages/about/index'
-import Home from './pages/home/index'
-import Cart from './pages/cart/index'
-import Contact from './pages/contact/index'
-import Category from './pages/product_category/index'
-import Detail from './pages/product_detail/index'
-import Overview from './pages/product_overview/index'
-import Login from "./pages/login";
-import Register from "./pages/register";
-import NotFound from "./pages/notFound";
-import OrderPage from "./pages/contact/components/OrderPage";
-import General from "./pages/contact/components/General";
+import About from './pages/about/index';
+import Home from './pages/home/index';
+import Cart from './pages/cart/index';
+import Contact from './pages/contact/index';
+import Category from './pages/product_category/index';
+import Detail from './pages/product_detail/index';
+import Overview from './pages/product_overview/index';
+import Login from './pages/login';
+import Register from './pages/register';
+import NotFound from './pages/notFound';
+import OrderPage from './pages/contact/components/OrderPage';
+import General from './pages/contact/components/General';
 
 // layout
-import RootLayout from "./layout/RootLayout";
-
-
+import RootLayout from './layout/RootLayout';
 
 // creating various routers
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     // rootlayout
-    <Route path='/' element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path=':category' element={<Category />} />
-      <Route path=':category/:detail' element={<Detail />} />
-      <Route path=':overview' element={<Overview />} />
-      <Route path='cart' element={<Cart />} />
+      <Route path="about" element={<About />} />
+      <Route path="category" element={<Category />} />
+      <Route path="detail" element={<Detail />} />
+      <Route path="overview" element={<Overview />} />
+      <Route path="cart" element={<Cart />} />
       {/* contact */}
-      <Route path='contact' element={<Contact />}>
+      <Route path="contact" element={<Contact />}>
         <Route path="general" element={<General />} />
         <Route path="order" element={<OrderPage />} />
       </Route>
       {/* login and register */}
-      <Route path='register' element={<Register />} />
-      <Route path='login' element={<Login />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
-)
-
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
